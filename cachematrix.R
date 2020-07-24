c29fab09a4165d795895cc330bbb7a95bd7233b9
LisaTai95 <66983169+LisaTai95@users.noreply.github.com>
2020-07-24 16:05:44 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,28 +4,10 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
- alr <- NULL
-  set <- function(y){
-    x<<-y
-    alr<<-NULL
-  }
-  get<-function(){x}
-  setinver <- function(inverse){alr<<-inverse}
-  getinver <- function(){alr}
-  list(set = set,get=get,setinver = setinver, getinver = getinver)
+
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        alr <- x$getinverse()
-          if(!is.null(inv)){
-                 message("obtener dato del cache")
-                  return(alr)
-  }
-         mat<-x$get()
-         alr<- solve(mat,...)
-          x$setinver(alr)
-  alr
 }
